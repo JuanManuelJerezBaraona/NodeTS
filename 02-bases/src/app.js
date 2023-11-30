4b7706fc8cd043a36a56e18d5be1e49b6ad9ca9d
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-11-30 12:33:26 -0300

    Add winston logger plugin

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -5,13 +5,18 @@ const { getAge, getUUID } = require('./plugins');
 // const { getUserById } = require('./js-foundation/03-callbacks');
 // const { getUserById } = require('./js-foundation/04-arrow');
 // const { buildMakePerson } = require('./js-foundation/05-factory')
-const getPokemonById = require('./js-foundation/06-promises');
+// const getPokemonById = require('./js-foundation/06-promises');
+const { buildLogger } = require('./plugins');
+
+const logger = buildLogger('app.js');
+
+logger.log('Hola mundo');
 
 
-getPokemonById(4)
-  .then( ( pokemon ) => console.log({ pokemon }) )
-  .catch( ( err ) => console.log( err ) )
-  .finally( () => console.log('Finalmente') );
+// getPokemonById(4)
+//   .then( ( pokemon ) => console.log({ pokemon }) )
+//   .catch( ( err ) => console.error( err ) )
+//   .finally( () => console.log('Finalmente') );
 
 
 
--- 02-bases/src/plugins/index.js
+++ 02-bases/src/plugins/index.js
@@ -3,8 +3,11 @@ const { getAge }  = require('../plugins/get-age.plugin');
 
 const { http } = require('../plugins/http-client.plugin');
 
+const buildLogger = require('../plugins/logger.plugin');
+
 module.exports = {
   getAge, 
   getUUID,
   http,
+  buildLogger,
 }
