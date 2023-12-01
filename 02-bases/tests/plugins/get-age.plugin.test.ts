9a0db2015cd0e841edf5e1177d70ddf2656d9cd4
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-01 18:37:50 -0300

    Fix error handling in http-client.plugin.ts

---
+++ 02-bases/tests/plugins/get-age.plugin.test.ts
@@ -0,0 +1,21 @@
+import { getAge } from "../../src/plugins";
+
+describe('plugins/get-age.plugin.ts', () => {
+
+    test('getAge() should return the age of a person', () => {
+        const birthdate = "1990-09-27";
+        const age = getAge(birthdate);
+
+        expect(typeof age).toBe('number');
+    });
+
+    test('getAge() should return current age', () => {
+        const birthdate = "1990-09-27";
+        const age = getAge(birthdate);
+
+        const calculatedAge = new Date().getFullYear() - new Date(birthdate).getFullYear();
+
+        expect(age).toBe(calculatedAge);
+    });
+
+});
