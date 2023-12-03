8358d45298dbc697ec4a6673dbdec61a910f33e9
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-03 10:17:39 -0300

    Refactor create table use case test

--- 04-multiplication/src/domain/use-cases/create-table.use-case.test.ts
+++ 04-multiplication/src/domain/use-cases/create-table.use-case.test.ts
@@ -10,12 +10,31 @@ describe('CreateTableUseCase', () => {
 
         const rows = table.split('\n');
 
-        console.log(table);
-
         expect(createTable).toBeInstanceOf(CreateTable);
         expect(table).toContain('2 x 1 = 2');
         expect(table).toContain('2 x 10 = 20');
         expect(rows.length).toBe(10);
+
+    });
+
+    test('should create table with custom values', () => {
+
+        const options = {
+            base: 3,
+            limit: 20
+        }
+
+        const createTable = new CreateTable();
+
+        const table = createTable.execute(options);
+
+        const rows = table.split('\n');
+
+        console.log(table);
+
+        expect(table).toContain('3 x 1 = 3');
+        expect(table).toContain('3 x 20 = 60');
+        expect(rows.length).toBe(options.limit);
 
     });
 
